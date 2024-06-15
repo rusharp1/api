a20371f622bc466d555645f7c72abe910b1dde88
rusharp <shop2930@naver.com>
2024-06-15 15:51:57 +0900

    로스트아크 캐릭터 출력

--- lostark_api/lostark_character_get.py
+++ lostark_api/lostark_character_get.py
@@ -1,82 +1,79 @@
 import requests
-import json
-import pprint
-from lostark_api_token import authorization_key
+from lostark_api_token import *
 
-# 로스트아크
-url =  "https://developer-lostark.game.onstove.com/"
-characterName = input("캐릭터 이름을 입력해 주세요 : ")
+def get_character_info(characterName):
+    # 계정의 모든 캐릭터 정보
+    link = f"/characters/{characterName}/siblings"
+    headers = {
+        'accept': 'application/json',
+        'authorization': authorization_key
+    }
 
-# 캐릭터 이름별로 기본 능력치 요약을 반환합니다.
-# link = f"armories/characters/{characterName}/profiles"
+    response = requests.get(lostark_url + link, headers=headers)
+    #오류 발생 시 예외를 던짐
+    response.raise_for_status()
+    return response.json()
 
-# 계정의 모든 캐릭터 정보
-link = f"characters/{characterName}/siblings"
-headers = {
-    'accept': 'application/json',
-    'authorization': authorization_key
-}
+def get_serverName(characters):
+    # 서버이름 목록 가져온 뒤 중복제거
+    ServerName_list = [character['ServerName'] for character in characters]
+    return list(set(ServerName_list))
 
-response = requests.get(url + link, headers=headers)
-test = response.json()
+def characters_by_server(characters, ServerName_list):
+    # 서버 별 캐릭터 목록으로 정리하기
+    # {서버명 : 캐릭터 목록 list}
+    char_info_list = {}
+    for servername in ServerName_list:
+        char_info_list[servername] = \
+            [char for char in characters if char['ServerName'] == servername]
+    return char_info_list
 
-# 서버이름 목록 가져온 뒤 중복제거
-ServerName_list = []
-for ServerName in test:
-    ServerName_list.append(ServerName['ServerName'])
+def get_profile_images(character):
+    # 캐릭터 이미지 가져오기 (API GET)
+    link = f"/armories/characters/{character['CharacterName']}/profiles"
+    headers = {
+        'accept': 'application/json',
+        'authorization': authorization_key
+    }
+    response = requests.get(lostark_url + link, headers=headers)
+    
+    # 캐릭터 이미지 중 'CharacterImage'에 해댕하는 내용만 가져옴
+    return response.json()['CharacterImage']
 
-ServerName_list = list(set(ServerName_list))
+def display_characters(char_info_list):
+    for key, value in char_info_list.items():
+        print("==================================================")
+        print("서버명 : {!r:10s} 캐릭터 수 : {}".format(key.replace("'", ""), len(value)))
+        print("==================================================")
+        for character in value:
+            
+            # 캐릭터 이미지 가져오기
+            profile_image = get_profile_images(character)
 
-# 서버 별 캐릭터 목록으로 정리하기
-# {서버명 : 캐릭터 목록 list}
-char_info_list = {}
-for servername in ServerName_list:
-    char_list = []
-    for n in range(len(test)):
-        if test[n]['ServerName'] == servername:
-            char_list.append(test[n])
-        char_info_list[servername]  = char_list
-
-
-# 서버이름 별 캐릭터 출력하기
-for key, value in char_info_list.items():
-    print("==================================================")
-    print("서버명 : {!r:10s} 캐릭터 수 : {}".format(key.replace("'", ""), len(value)))
-    print("==================================================")
-    for v in value:
-        # 캐릭터 이미지 가져오기 (API GET)
-        link = f"armories/characters/{v['CharacterName']}/profiles"
-        headers = {
-            'accept': 'application/json',
-            'authorization': authorization_key
-        }
-        response = requests.get(url + link, headers=headers)
-        char_img = response.json()
-        
-        # 캐릭터 정보 출력
-        print("캐릭터      : {}".format(char_img['CharacterImage']))
-        print("캐릭터명    : {} ".format(v['CharacterName']))
-        print("캐릭터 레벨 : {} ".format(v['CharacterLevel']))
-        print("클래스      : {} ".format(v['CharacterClassName']))
-        print("아이템 레벨 : {} ".format(v['ItemMaxLevel']))
-        print()
+            # 캐릭터 정보 출력
+            print("캐릭터 프로필 : {}".format(profile_image))
+            print("캐릭터명      : {} ".format(character['CharacterName']))
+            print("캐릭터 레벨   : {} ".format(character['CharacterLevel']))
+            print("클래스        : {} ".format(character['CharacterClassName']))
+            print("아이템 레벨   : {} ".format(character['ItemMaxLevel']))
+            print()
 
 
-# link = f"armories/characters/{characterName}/profiles"
+def main():
+    # 로스트아크
+    characterName = input("캐릭터 이름을 입력해 주세요 : ")
 
-# 두레이
-# url =  "https://api.dooray.com/"
-# api_key = "dooray-api jwvixmghaqn2:8irV3QhvT5-ldxntlIx2Mw"
-# link = "project/v1/projects"
+    # 캐릭터 이름별로 기본 능력치 요약을 반환.
+    # link = f"armories/characters/{characterName}/profiles"
 
-# # 프로젝트 목록을 받아옴.
-# headers = {
-#     "Authorization" : "dooray-api jwvixmghaqn2:8irV3QhvT5-ldxntlIx2Mw",
-#     "Content-Type" : "application/json"}
+    # 캐릭터 전체 정보 가져오기
+    characters = get_character_info(characterName)
+    # 캐릭터 서버 정보 가져오기
+    ServerName_list = get_serverName(characters)
+    # 포함된 서버 별 캐릭터 목록 가져오기
+    char_info_list = characters_by_server(characters, ServerName_list)
+    # 서버이름 별 캐릭터 출력하기
+    display_characters(char_info_list)
 
-# params = {f
-#     "name" : "jtest"
-# }
-
-
-
+if __name__== "__main__":
+   main()
